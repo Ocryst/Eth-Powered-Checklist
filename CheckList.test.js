23288a98689f85face0a9f23730512b30a4b8f82
Ocryst <66696517+Ocryst@users.noreply.github.com>
2020-07-14 13:55:32 -0400

    Add files via upload

---
+++ CheckList.test.js
@@ -0,0 +1,44 @@
+const CheckList = artifacts.require('./CheckList.sol')
+
+contract('CheckList', (accounts) => {
+  before(async () => {
+    this.checkList = await CheckList.deployed()
+  })
+
+  it('deploys successfully', async () => {
+    const address = await this.checkList.address
+    assert.notEqual(address, 0x0)
+    assert.notEqual(address, '')
+    assert.notEqual(address, null)
+    assert.notEqual(address, undefined)
+  })
+
+  it('lists tasks', async () => {
+    const taskCount = await this.checkList.taskCount()
+    const task = await this.checkList.tasks(taskCount)
+    assert.equal(task.id.toNumber(), taskCount.toNumber())
+    assert.equal(task.content, 'Check out dappuniversity.com')
+    assert.equal(task.completed, false)
+    assert.equal(taskCount.toNumber(), 1)
+  })
+
+  it('creates tasks', async () => {
+    const result = await this.checkList.createTask('A new task')
+    const taskCount = await this.checkList.taskCount()
+    assert.equal(taskCount, 2)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'A new task')
+    assert.equal(event.completed, false)
+  })
+
+  it('toggles task completion', async () => {
+    const result = await this.checkList.toggleCompleted(1)
+    const task = await this.checkList.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
+  })
+
+})
